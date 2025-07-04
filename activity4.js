cb42db9ab7f9a2009ff47b2db9e6b52a053f05db
0512051205 <jandermaliktan06@gmail.com>
2025-07-04 12:01:26 +0800

    Create activity4.js

---
+++ activity4.js
@@ -0,0 +1,11 @@
+let firstFavNum = 11;
+let secondFavNum = 27;
+
+console.log (firstFavNum > secondFavNum);
+console.log (firstFavNum < secondFavNum);
+console.log (firstFavNum >= secondFavNum);
+console.log (firstFavNum <= secondFavNum);
+console.log (firstFavNum === secondFavNum);
+console.log (firstFavNum == secondFavNum);
+console.log (firstFavNum !== secondFavNum);
+console.log (firstFavNum != secondFavNum);

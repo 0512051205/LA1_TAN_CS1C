78630e54fd222229aa6e6fa430b10457d04374de
0512051205 <jandermaliktan06@gmail.com>
2025-07-04 12:02:10 +0800

    Create activity5.js

---
+++ activity5.js
@@ -0,0 +1,8 @@
+let favActorFirstName = "jander ";
+let favActorLastName = "miguel";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = My Favorite Actor is ${uppercase}, ;
+message  += his best show is true beuty;
+
+console.log(message);

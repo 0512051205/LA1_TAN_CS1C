f2ca00421a0e1f8287be01dc2d7e7e2b91032636
0512051205 <jandermaliktan06@gmail.com>
2025-07-04 11:58:34 +0800

    Create activity1.js

---
+++ activity1.js /activity1.js
@@ -0,0 +1,9 @@
+let name = "JANDER";
+let YouWannaBecomeInYourLife = "programmer";
+let gender = "MALE";
+let twitterHandle = "jandermaliktan06@gmail.com";
+
+console.log (name);
+console.log (YouWannaBecomeInYourLife);
+console.log (gender);
+console.log (twitterHandle);

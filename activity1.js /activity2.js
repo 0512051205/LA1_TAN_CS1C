82d625b020f59e258ef7cd1db6e9ac438ce30e0e
0512051205 <jandermaliktan06@gmail.com>
2025-07-04 11:59:35 +0800

    Create activity2.js

---
+++ activity1.js /activity2.js
@@ -0,0 +1,9 @@
+let firstFavNum = 10;
+let secondFavNum = 18;
+
+console.log (firstFavNum + secondFavNum);
+console.log (firstFavNum - secondFavNum);
+console.log (firstFavNum * secondFavNum);
+console.log (firstFavNum / secondFavNum);
+console.log (firstFavNum % secondFavNum);
+console.log (firstFavNum ** secondFavNum);
